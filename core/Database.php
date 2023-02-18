708a38f56e8baa9df1600ee7d433de1ddf4ea998
MishaR <mesharoman12@gmail.com>
2023-02-18 15:17:41 +0200

    Added dotenv library for database connection params

--- core/Database.php
+++ core/Database.php
@@ -6,18 +6,21 @@ use PDO;
 
 class Database
 {
-	private $host = 'localhost';
-	private $db_name = 'php_api';
-	private $username = 'root';
-	private $password = 'root';
 	private $conn;
 
 	public function connect()
 	{
+		$dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+		$dotenv->load();
+
 		$this->conn = null;
+		$host = $_ENV['DB_HOST'];
+		$db_name = $_ENV['DB_DATABASE'];
+		$username = $_ENV['DB_USER'];
+		$password = $_ENV['DB_PASSWORD'];
 
 		try {
-			$this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+			$this->conn = new PDO('mysql:host=' . $host . ';dbname=' . $db_name, $username, $password);
 	        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (PDOException $e) {
 			echo 'Connection error: ' . $e->getMessage();
